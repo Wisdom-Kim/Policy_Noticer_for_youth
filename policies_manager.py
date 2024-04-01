63eba9fa33b01ee7abe285afc7bbefc4f3b061f8
Wisdom-Kim <cocoa389@cau.ac.kr>
2024-04-01 20:08:46 +0900

    ✨ feat : database.txt에 정책 ID 추가
    get_fid_id 함수로 정책별 ID를 구해
    database.txt파일에 추가모드(a)로 저장함

--- policies_manager.py
+++ policies_manager.py
@@ -4,61 +4,57 @@ from feed import Feed
 from selenium.webdriver.common.by import By
 import time
 
-def get_last_index(url):
-    #TODO 해당 함수를 '괄호 안 요소를 추출하는 함수'로 범용화시키기
-    driver.get(url)
-    fn = driver.find_element(By.XPATH,"/html/body/div[3]/div/div[1]/div/div[2]/form/div/div[4]/div[1]/div[2]/a[9]").get_attribute("onclick")
-    
-    def find_index():
-        #함수 매개변자를 찾아 인덱스를 반환하는 함수
-        first_index = fn.find('(')
-        last_index=fn.find(')')
-        return fn[first_index+1:last_index]
-    
-    return find_index()
+def get_page_component(component) -> str:
+    # 피드 컴포넌트의 속성 중 마지막 페이지가 속한 부분을 반환
+    fn = component.find_element(By.XPATH,'/html/body/div[3]/div/div[1]/div/div[2]/form/div/div[4]/div[1]/div[2]/a[9]').get_attribute("onclick")
+    return fn
 
-def find_id(feed):
-    feed.find_element(By.CLASS_NAME,"item").get_attribute("onclick")
+def get_fid_component(item) -> str:
+    # 피드 컴포넌트의 속성 중 피드의 id가 속한 부분을 반환
+    return item.find_element(By.CLASS_NAME,"item-overlay").get_attribute("onclick")
 
-def create_feed(content):
+def remove_bracket(component) ->str:
+    first_index = component.find('(')
+    last_index=component.find(')')
+    return component[first_index+1:last_index].strip('\'')
+
+def create_policy(feed) -> object:
+    #컴포넌트 내 정보를 기반으로 객체 생성
     content = feed.find_element(By.CLASS_NAME,"content")
     try:
         state= content.find_element(By.CLASS_NAME,"state").text
         if(state=='모집중'):
-            category= content.find_element(By.CLASS_NAME,"cate").text
+            category= feed.find_element(By.CLASS_NAME,"cate").text
             title= content.find_element(By.CLASS_NAME,"name").text
             src = feed.find_element(By.TAG_NAME,"img").get_attribute("src")
-            #TODO 피드의 ID 추출하기
-            #Feed(title,category,src,fid)
+            fid = remove_bracket(get_fid_component(feed))
+            return Feed(title,category,src,fid)
     except Exception as e:
         # 모집 중이 아닌 기업은 pass
         pass
+
+def write_id(id) -> None:
+    with open('database.txt','a') as f:
+        f.write(id+"\n")
 
 driver = webdriver.Chrome()
-BASE_URL = 'https://youth.seoul.go.kr'
 URL = 'https://youth.seoul.go.kr/infoData/sprtInfo/list.do?key=2309130006'
-#user=User(None,)
-driver.get(URL)
+
+base = driver.get(URL)
+
 feeds = driver.find_elements(By.CLASS_NAME,"feed-item")
+recruit_filter_btn = driver.find_element(By.XPATH,'/html/body/div[3]/div/div[1]/div/div[2]/form/div/div[2]/div[4]/ul/li[5]/a').click()
+time.sleep(1)
+search_btn = driver.find_element(By.XPATH,'/html/body/div[3]/div/div[1]/div/div[2]/form/div/div[2]/div[4]/ul/li[5]/div/ul/li[2]').click()
+time.sleep(1)
 
-
-
-
+policy_list =[]
 for feed in feeds:
-    content = feed.find_element(By.CLASS_NAME,"content")
-    try:
-        state= content.find_element(By.CLASS_NAME,"state").text
-        if(state=='모집중'):
-            category= content.find_element(By.CLASS_NAME,"cate").text
-            title= content.find_element(By.CLASS_NAME,"name").text
-            src = feed.find_element(By.TAG_NAME,"img").get_attribute("src")
-            
-    except Exception as e:
-        # 모집 중이 아닌 기업은 pass
-        pass
-    
-#print(feeds)
-#TODO 함수 리팩토링
-last_index = get_last_index(URL)
+    policy_list.append(create_policy(feed))
+    fid = remove_bracket(get_fid_component(feed))
+    write_id(fid)
 
+print(policy_list)
+#print(feeds)
+last_index = remove_bracket(get_page_component(driver))
 time.sleep(1000)
